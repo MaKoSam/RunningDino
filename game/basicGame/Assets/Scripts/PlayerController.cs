36946dd83793dfca567e8c6d051929e2269252a4
L180300104-Alex <61035234+L180300104-Alex@users.noreply.github.com>
2021-08-19 13:02:14 +0800

    build

--- game/basicGame/Assets/Scripts/PlayerController.cs
+++ game/basicGame/Assets/Scripts/PlayerController.cs
@@ -6,41 +6,41 @@
  */
 public class PlayerController : MonoBehaviour
 {
-  private Rigidbody2D rigidbody2d;
-  private int health;
-  private int score;
-  private bool canJump;
+    private Rigidbody2D rigidbody2d;
+    private int health;
+    private int score;
+    private int canJump = 2;
 
-  /*
-   * Apply initial health and also store the Rigidbody2D reference for
-   * future because GetComponent<T> is relatively expensive.
-   */
-  private void Start()
-  {
-    health = 6;
-    rigidbody2d = GetComponent<Rigidbody2D>();
-  }
-
-  /*
-   * Remove one health unit from player and if health becomes 0, change
-   * scene to the end game scene.
-   */
-  public void Damage()
-  {
-    health -= 1;
-
-    if(health < 1)
+    /*
+     * Apply initial health and also store the Rigidbody2D reference for
+     * future because GetComponent<T> is relatively expensive.
+     */
+    private void Start()
     {
-      Application.LoadLevel("EndGame");
+        health = 6;
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
-  }
+
+    /*
+     * Remove one health unit from player and if health becomes 0, change
+     * scene to the end game scene.
+     */
+    public void Damage()
+    {
+        health -= 1;
+
+        if (health < 1)
+        {
+            Application.LoadLevel("EndGame");
+        }
+    }
 
 
-   /*
-    * Increase the score each time the coin has been collected
-    */
-   public void CollectCoin()
-   { 
+    /*
+     * Increase the score each time the coin has been collected
+     */
+    public void CollectCoin()
+    {
         score += 1;
         ApplicationInstance.SharedInstance.SetScore(score);
     }
@@ -63,35 +63,35 @@ public class PlayerController : MonoBehaviour
     }
 
 
-   public Vector3 GetPosition()
+    public Vector3 GetPosition()
     {
         return rigidbody2d.position;
     }
 
-  /*
-   * Poll keyboard for when the up arrow is pressed. If the player can jump
-   * (is on the ground) then add force to the cached Rigidbody2D component.
-   * Finally unset the canJump flag so the player has to wait to land before
-   * another jump can be triggered.
-   */
-  private void Update()
-  {
-    if(Input.GetKeyDown(KeyCode.UpArrow))
+    /*
+     * Poll keyboard for when the up arrow is pressed. If the player can jump
+     * (is on the ground) then add force to the cached Rigidbody2D component.
+     * Finally unset the canJump flag so the player has to wait to land before
+     * another jump can be triggered.
+     */
+    private void Update()
     {
-      if(canJump == true)
-      {
-        rigidbody2d.AddForce(new Vector2(0, 500));
-        canJump = false;
-      }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (canJump > 0)
+            {
+                rigidbody2d.AddForce(new Vector2(0, 500));
+                canJump -= 1;
+            }
+        }
     }
-  }
 
-  /*
-   * If the player has collided with the ground, set the canJump flag so that
-   * the player can trigger another jump.
-   */
-  private void OnCollisionEnter2D(Collision2D other)
-  {
-    canJump = true;
-  }
+    /*
+     * If the player has collided with the ground, set the canJump flag so that
+     * the player can trigger another jump.
+     */
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        canJump = 2;
+    }
 }
